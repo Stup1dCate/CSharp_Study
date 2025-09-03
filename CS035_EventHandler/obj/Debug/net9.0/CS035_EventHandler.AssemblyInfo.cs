fa91fb67fd7fffd4ed32448c1845b8c11cdf7a2d
Tung Vu <tungvu11305@gmail.com>
2025-09-03 14:48:06 +0700

    upd

--- CS035_EventHandler/obj/Debug/net9.0/CS035_EventHandler.AssemblyInfo.cs
+++ CS035_EventHandler/obj/Debug/net9.0/CS035_EventHandler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS035_EventHandler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3ea4abef16db4855d44ed58706a8c6f9d52cff")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS035_EventHandler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS035_EventHandler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
