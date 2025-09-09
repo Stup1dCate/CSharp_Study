73255014fad730cbe79b2afc045f951088d39d52
Tung Vu <tungvu11305@gmail.com>
2025-09-09 14:40:26 +0700

    Study - C# - File/Directory

---
+++ CS041_File/CS041_1_file/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+/*
+1. Đọc thông tin ổ đĩa
+2. Làm việc với ổ đĩa bằng lớp Directory
+3. Đường dẫn file và thư mục với lớp Path
+4. làm việc với thư mục bằng lớp File (đọc - xoá - copy - ...)
+5. Đọc ghi file với FileStream
+6. Lưu trữ và đọc dữ liệu đối tượng từ file
+*/
+/*
+Thuộc tính lớp DriveInfo:
+1. IsReady
+-> true ổ đĩa ở trạng thái sẵn sàng (hoạt động) – ví dụ ổ đĩa CD nếu không có đĩa thì không sẵn sàng false
+
+2. DriveType
+-> Kiểu ổ đĩa (System.IO.DriveType): CDRom, Fixed (ổ cố định - gắn trong máy), Network (ổ mạng), NoRootDirectory, Ram, Removable (USB), Unknown
+
+3. VolumeLabel
+-> Nhãn đĩa (nhẵn thư mục khi người dùng tạo một thư mục mới - ví dụ "New Volume")
+
+4. DriveFormat
+-> Chuỗi cho biết định dạng đĩa: NTFS (tệp hệ thống mặc định của Windown), FAT32, FAT, devfs ...
+
+5. AvailableFreeSpace
+-> Số byte có hiệu lực còn trống (theo hạn ngạch người dùng)
+
+6. TotalFreeSpace
+-> Số byte còn trống
+
+7. TotalSize
+-> Tổng số byte trên đĩa
+*/
+namespace CS041_1_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // truy cập các thuộc tính của ổ đĩa
+            DriveInfo dr = new DriveInfo("D");
+            Console.WriteLine($"Drive: {dr.Name}");              // tên
+            Console.WriteLine($"DriveType: {dr.DriveType}");     // kiểu
+            Console.WriteLine($"Label: {dr.VolumeLabel}");       // nhãn
+            Console.WriteLine($"Format: {dr.DriveFormat}");      // định dạng
+            Console.WriteLine($"Size: {dr.TotalSize}");          // tổng dung lượng
+            Console.WriteLine($"Free: {dr.TotalFreeSpace}");     // dung lượng khả dụng còn lại
+
+
+            // lấy ra tất cả ổ đĩa có trên máy này:
+            Console.WriteLine("\nHien thi thong tin tat ca cac o dia trong may: ");
+            var drives = DriveInfo.GetDrives();
+            foreach (var drive in drives)
+            {
+                Console.WriteLine($"Drive: {drive.Name}");
+                Console.WriteLine($"DriveType: {drive.DriveType}");
+                Console.WriteLine($"Label: {drive.VolumeLabel}");
+                Console.WriteLine($"Format: {drive.DriveFormat}");
+                Console.WriteLine($"Size: {drive.TotalSize}");
+                Console.WriteLine($"Free: {drive.TotalFreeSpace}");
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ CS041_File/CS041_2_directory/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+
+namespace CS041_2_directory
+{
+    /*
+        DIRECTORY:
+    Exists(path)	        Kiểm tra xem thư mục có tồn tại (true) hay không (false).
+    CreateDirectory(path)	Tạo thư mục, trả về đối tượng System.IO.DirectoryInfo chứa thông tin thư mục.
+    Delete(path)	        Xóa thư mục.
+    GetFiles(path)	        Lấy các file trong thư mục.
+    GetDirectories(path)	Lấy các thư mục trong thư mục.
+    Move(src, des)	        Di chuyển thư mục.
+    */
+
+    class Program
+    {
+        // Sử dụng hàm đệ quy để liệt kê hết tất cả các file và thư mục con của một thư mục:
+        static void ListFileDirectory(string path)
+        {
+            String[] directories = System.IO.Directory.GetDirectories(path);
+            String[] files = System.IO.Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+                ListFileDirectory(directory); // Đệ quy
+            }
+        }
+        static void Main(string[] args)
+        {
+            // kiểm tra sự tồn tại của thư mục:
+            string path = "Xyz";
+            Directory.CreateDirectory(path);    // nếu không khởi tạo thư mục từ trước hoặc không có lệnh khởi tạo thì thư mục "Xyz" không tồn tại để kiểm tra
+            Directory.Delete(path);             // xoá đường dẫn nhập vào
+            if (Directory.Exists(path)) Console.WriteLine($"{path} - ton tai");
+            else Console.WriteLine($"{path} - khong ton tai");
+
+            // lấy ra tất cả các file trong cùng 1 thư mục:
+            string path_1 = "obj";
+            var files = Directory.GetFiles(path_1);             // lấy ra tất cả các thư mục con trực tiếp trong thư mục obj
+            var directories = Directory.GetDirectories(path_1);   // lấy ra tất cả các thư mục con là subdirector (thư mục chứa các thư mục con khác - nó là con của obj) trong thư mục obj
+
+            Console.WriteLine("Cac thu muc con subdirector trong obj la:");
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+            /* nếu ở đây muốn liệt kê tiếp các file con trong thư mục Debug (obj\Debug\) thì có thể tạo một đối tượng chứa đường dẫn
+            là string path_2 = "Debug" và chạy hàm đệ quy phía trên
+            */
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Cac thu muc con truc tiep trong obj la:");
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+
+            // Liệt kê hết tất cả các file và thư mục con của một thư mục:
+            Console.WriteLine("\nTat ca cac file - thu muc con nam trong thu muc \"obj\" la:");
+            ListFileDirectory(path_1);
+
+            // ví dụ chỉ muốn duyệt mình các file con của Debug (nằm trong obj):
+            string path_2 = @"obj\Debug"; 
+            /*
+            vì file Debug là thư mục con không cùng cấp với obj nên phải ghi là @"obj/Debug" 
+            ghi "obj\Debug" => không có @ để xác định đường link -> sai
+            ghi "Debug" hay @"Debug" => không chứa thư mục cha là "obj" -> sai
+            */
+            Console.WriteLine("\nTat ca cac file - thu muc con nam trong thu muc \"Debug\" la:");
+            ListFileDirectory(path_2);
+
+            
+
+        }
+    }
+}
