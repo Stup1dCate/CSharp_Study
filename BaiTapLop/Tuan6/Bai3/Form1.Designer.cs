08dc0986bc721cfa3fec6da15b225850d7beff46
Tung Vu <tungvu11305@gmail.com>
2025-10-14 14:01:12 +0700

    week 6 exerc - ex3

---
+++ BaiTapLop/Tuan6/Bai3/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Bai3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn15 = new System.Windows.Forms.Button();
+            this.btn14 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.lblThanhTien = new System.Windows.Forms.Label();
+            this.txtThanhTien = new System.Windows.Forms.TextBox();
+            this.btnChon = new System.Windows.Forms.Button();
+            this.btnHuyBo = new System.Windows.Forms.Button();
+            this.btnKetThuc = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn1.Location = new System.Drawing.Point(12, 93);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(110, 62);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn2.Location = new System.Drawing.Point(140, 93);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(110, 62);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "1";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn3.Location = new System.Drawing.Point(288, 93);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(122, 62);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "1";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn4.Location = new System.Drawing.Point(452, 93);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(110, 62);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "1";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn5.Location = new System.Drawing.Point(586, 93);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(110, 62);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "1";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn10
+            // 
+            this.btn10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn10.Location = new System.Drawing.Point(586, 188);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(110, 62);
+            this.btn10.TabIndex = 9;
+            this.btn10.Text = "1";
+            this.btn10.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn9.Location = new System.Drawing.Point(452, 188);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(110, 62);
+            this.btn9.TabIndex = 8;
+            this.btn9.Text = "1";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn8.Location = new System.Drawing.Point(288, 188);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(122, 62);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "1";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn6.Location = new System.Drawing.Point(12, 188);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(110, 62);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "1";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn15
+            // 
+            this.btn15.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn15.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn15.Location = new System.Drawing.Point(586, 287);
+            this.btn15.Name = "btn15";
+            this.btn15.Size = new System.Drawing.Size(110, 62);
+            this.btn15.TabIndex = 14;
+            this.btn15.Text = "1";
+            this.btn15.UseVisualStyleBackColor = true;
+            // 
+            // btn14
+            // 
+            this.btn14.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn14.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn14.Location = new System.Drawing.Point(452, 287);
+            this.btn14.Name = "btn14";
+            this.btn14.Size = new System.Drawing.Size(110, 62);
+            this.btn14.TabIndex = 13;
+            this.btn14.Text = "1";
+            this.btn14.UseVisualStyleBackColor = true;
+            // 
+            // btn13
+            // 
+            this.btn13.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn13.Location = new System.Drawing.Point(288, 287);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(122, 62);
+            this.btn13.TabIndex = 12;
+            this.btn13.Text = "1";
+            this.btn13.UseVisualStyleBackColor = true;
+            // 
+            // btn12
+            // 
+            this.btn12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn12.Location = new System.Drawing.Point(140, 287);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(110, 62);
+            this.btn12.TabIndex = 11;
+            this.btn12.Text = "1";
+            this.btn12.UseVisualStyleBackColor = true;
+            // 
+            // btn11
+            // 
+            this.btn11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn11.Location = new System.Drawing.Point(12, 287);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(110, 62);
+            this.btn11.TabIndex = 10;
+            this.btn11.Text = "1";
+            this.btn11.UseVisualStyleBackColor = true;
+            // 
+            // lblThanhTien
+            // 
+            this.lblThanhTien.AutoSize = true;
+            this.lblThanhTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblThanhTien.Location = new System.Drawing.Point(57, 390);
+            this.lblThanhTien.Name = "lblThanhTien";
+            this.lblThanhTien.Size = new System.Drawing.Size(107, 20);
+            this.lblThanhTien.TabIndex = 15;
+            this.lblThanhTien.Text = "Thành Tiền : ";
+            // 
+            // txtThanhTien
+            // 
+            this.txtThanhTien.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtThanhTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtThanhTien.Location = new System.Drawing.Point(224, 379);
+            this.txtThanhTien.Name = "txtThanhTien";
+            this.txtThanhTien.Size = new System.Drawing.Size(551, 36);
+            this.txtThanhTien.TabIndex = 16;
+            // 
+            // btnChon
+            // 
+            this.btnChon.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnChon.Location = new System.Drawing.Point(76, 447);
+            this.btnChon.Name = "btnChon";
+            this.btnChon.Size = new System.Drawing.Size(152, 55);
+            this.btnChon.TabIndex = 17;
+            this.btnChon.Text = "Chọn";
+            this.btnChon.UseVisualStyleBackColor = true;
+            // 
+            // btnHuyBo
+            // 
+            this.btnHuyBo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnHuyBo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnHuyBo.Location = new System.Drawing.Point(291, 447);
+            this.btnHuyBo.Name = "btnHuyBo";
+            this.btnHuyBo.Size = new System.Drawing.Size(162, 55);
+            this.btnHuyBo.TabIndex = 18;
+            this.btnHuyBo.Text = "Huỷ bỏ";
+            this.btnHuyBo.UseVisualStyleBackColor = true;
+            // 
+            // btnKetThuc
+            // 
+            this.btnKetThuc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnKetThuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnKetThuc.Location = new System.Drawing.Point(499, 447);
+            this.btnKetThuc.Name = "btnKetThuc";
+            this.btnKetThuc.Size = new System.Drawing.Size(157, 55);
+            this.btnKetThuc.TabIndex = 19;
+            this.btnKetThuc.Text = "Kết thúc";
+            this.btnKetThuc.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(708, 72);
+            this.panel1.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Coral;
+            this.label2.Location = new System.Drawing.Point(285, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "MÀN ẢNH";
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.btn7.Location = new System.Drawing.Point(140, 188);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(110, 62);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "1";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 514);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnKetThuc);
+            this.Controls.Add(this.btnHuyBo);
+            this.Controls.Add(this.btnChon);
+            this.Controls.Add(this.txtThanhTien);
+            this.Controls.Add(this.lblThanhTien);
+            this.Controls.Add(this.btn15);
+            this.Controls.Add(this.btn14);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn15;
+        private System.Windows.Forms.Button btn14;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Label lblThanhTien;
+        private System.Windows.Forms.TextBox txtThanhTien;
+        private System.Windows.Forms.Button btnChon;
+        private System.Windows.Forms.Button btnHuyBo;
+        private System.Windows.Forms.Button btnKetThuc;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn7;
+    }
+}
+
---
+++ BaiTapLop/Tuan6/Bai3/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai3
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<int, Button> seatButtons;
+        private HashSet<int> selectedSeats;
+        private HashSet<int> soldSeats;
+        private Dictionary<int, int> seatPrices;
+
+        // Giá vé theo lô
+        private const int PRICE_A = 10000;
+        private const int PRICE_B = 15000;
+        private const int PRICE_C = 20000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSeats();
+        }
+
+        private void InitializeSeats()
+        {
+            seatButtons = new Dictionary<int, Button>
+            {
+                {1, btn1}, {2, btn2}, {3, btn3}, {4, btn4}, {5, btn5},
+                {6, btn6}, {7, btn7}, {8, btn8}, {9, btn9}, {10, btn10},
+                {11, btn11}, {12, btn12}, {13, btn13}, {14, btn14}, {15, btn15}
+            };
+
+            selectedSeats = new HashSet<int>();
+            soldSeats = new HashSet<int>();
+
+            // Khởi tạo giá vé cho từng ghế
+            seatPrices = new Dictionary<int, int>
+            {
+                // Lô A: ghế 1-5
+                {1, PRICE_A}, {2, PRICE_A}, {3, PRICE_A}, {4, PRICE_A}, {5, PRICE_A},
+                // Lô B: ghế 6-10
+                {6, PRICE_B}, {7, PRICE_B}, {8, PRICE_B}, {9, PRICE_B}, {10, PRICE_B},
+                // Lô C: ghế 11-15
+                {11, PRICE_C}, {12, PRICE_C}, {13, PRICE_C}, {14, PRICE_C}, {15, PRICE_C}
+            };
+
+            // Đặt text cho các button và gán sự kiện click
+            foreach (var seat in seatButtons)
+            {
+                seat.Value.Text = seat.Key.ToString();
+                seat.Value.BackColor = Color.White; // Màu trắng cho ghế chưa bán
+                seat.Value.Click += SeatButton_Click;
+            }
+
+            // Khởi tạo textbox thành tiền
+            txtThanhTien.Text = "0";
+            txtThanhTien.ReadOnly = true;
+
+            // Gán sự kiện cho các button chức năng
+            btnChon.Click += btnChon_Click;
+            btnHuyBo.Click += btnHuyBo_Click;
+            btnKetThuc.Click += btnKetThuc_Click;
+        }
+
+        private void SeatButton_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            int seatNumber = int.Parse(clickedButton.Text);
+
+            // Kiểm tra nếu ghế đã bán
+            if (soldSeats.Contains(seatNumber))
+            {
+                MessageBox.Show($"Ghế số {seatNumber} đã được bán!", "Thông báo",
+                              MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            // Xử lý chọn/bỏ chọn ghế
+            if (selectedSeats.Contains(seatNumber))
+            {
+                // Bỏ chọn ghế - đổi về màu trắng
+                selectedSeats.Remove(seatNumber);
+                clickedButton.BackColor = Color.White;
+            }
+            else
+            {
+                // Chọn ghế - đổi sang màu xanh
+                selectedSeats.Add(seatNumber);
+                clickedButton.BackColor = Color.Blue;
+            }
+
+            // Cập nhật tổng tiền tạm tính
+            UpdateTotalPrice();
+        }
+
+        private void UpdateTotalPrice()
+        {
+            int total = 0;
+            foreach (int seat in selectedSeats)
+            {
+                total += seatPrices[seat];
+            }
+            txtThanhTien.Text = total.ToString();
+        }
+
+        private void btnChon_Click(object sender, EventArgs e)
+        {
+            if (selectedSeats.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn ít nhất một ghế!", "Thông báo",
+                              MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Xác nhận thanh toán
+            DialogResult result = MessageBox.Show(
+                $"Bạn có chắc chắn muốn mua {selectedSeats.Count} vé?\nTổng tiền: {txtThanhTien.Text} VNĐ",
+                "Xác nhận thanh toán",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                // Đánh dấu ghế đã bán và đổi màu vàng
+                foreach (int seat in selectedSeats)
+                {
+                    soldSeats.Add(seat);
+                    seatButtons[seat].BackColor = Color.Yellow;
+                }
+
+                // Hiển thị thông báo thành công
+                MessageBox.Show($"Thanh toán thành công!\nSố vé: {selectedSeats.Count}\nTổng tiền: {txtThanhTien.Text} VNĐ",
+                              "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Xóa danh sách ghế đang chọn
+                selectedSeats.Clear();
+                txtThanhTien.Text = "0";
+            }
+        }
+
+        private void btnHuyBo_Click(object sender, EventArgs e)
+        {
+            if (selectedSeats.Count == 0)
+            {
+                MessageBox.Show("Không có ghế nào đang được chọn!", "Thông báo",
+                              MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            // Xác nhận hủy bỏ
+            DialogResult result = MessageBox.Show(
+                "Bạn có chắc chắn muốn hủy bỏ các ghế đang chọn?",
+                "Xác nhận hủy bỏ",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                // Đổi màu ghế đang chọn về trắng
+                foreach (int seat in selectedSeats)
+                {
+                    seatButtons[seat].BackColor = Color.White;
+                }
+
+                // Xóa danh sách ghế đang chọn
+                selectedSeats.Clear();
+                txtThanhTien.Text = "0";
+
+                MessageBox.Show("Đã hủy bỏ các ghế đang chọn!", "Thông báo",
+                              MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnKetThuc_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Bạn có chắc chắn muốn kết thúc chương trình?",
+                "Xác nhận kết thúc",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        // Phương thức tiện ích để hiển thị thông tin về các lô ghế
+        private void ShowSeatInfo()
+        {
+            string info = "THÔNG TIN GIÁ VÉ:\n";
+            info += $"Lô A (ghế 1-5): {PRICE_A:N0} VNĐ/vé\n";
+            info += $"Lô B (ghế 6-10): {PRICE_B:N0} VNĐ/vé\n";
+            info += $"Lô C (ghế 11-15): {PRICE_C:N0} VNĐ/vé\n\n";
+            info += "CHÚ THÍCH MÀU SẮC:\n";
+            info += "• Màu trắng: Ghế trống\n";
+            info += "• Màu xanh: Ghế đang chọn\n";
+            info += "• Màu vàng: Ghế đã bán";
+
+            MessageBox.Show(info, "Thông tin rạp chiếu phim",
+                          MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Sự kiện Load form để hiển thị thông tin
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowSeatInfo();
+        }
+    }
+}
