9aac2daa43c96dbb52a398c487e41c752dafc47e
Tung Vu <tungvu11305@gmail.com>
2025-11-15 13:34:36 +0700

    study LinQ

---
+++ BaiTapLop/Tuan11/LinQtoObject/LinQtoObject/FromMain.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace LinQtoObject
+{
+    partial class FromMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnGiatricotrongA = new System.Windows.Forms.Button();
+            this.btnGiatribe4nho8 = new System.Windows.Forms.Button();
+            this.btnGiatritangdan = new System.Windows.Forms.Button();
+            this.btnGiatrilonhon4 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMangA = new System.Windows.Forms.TextBox();
+            this.txtMangB = new System.Windows.Forms.TextBox();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTimsvcomatrongdanhsach = new System.Windows.Forms.Button();
+            this.btnChuoichuae = new System.Windows.Forms.Button();
+            this.btnManglonhon4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1076, 54);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(348, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(395, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LẬP TRÌNH LINQ TO OBJECT";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnGiatricotrongA);
+            this.panel2.Controls.Add(this.btnGiatribe4nho8);
+            this.panel2.Controls.Add(this.btnGiatritangdan);
+            this.panel2.Controls.Add(this.btnGiatrilonhon4);
+            this.panel2.Controls.Add(this.tableLayoutPanel1);
+            this.panel2.Location = new System.Drawing.Point(12, 60);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1052, 285);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnGiatricotrongA
+            // 
+            this.btnGiatricotrongA.Location = new System.Drawing.Point(807, 165);
+            this.btnGiatricotrongA.Name = "btnGiatricotrongA";
+            this.btnGiatricotrongA.Size = new System.Drawing.Size(207, 62);
+            this.btnGiatricotrongA.TabIndex = 1;
+            this.btnGiatricotrongA.Text = "Tìm giá trị có trong a";
+            this.btnGiatricotrongA.UseVisualStyleBackColor = true;
+            this.btnGiatricotrongA.Click += new System.EventHandler(this.btnGiatricotrongA_Click);
+            // 
+            // btnGiatribe4nho8
+            // 
+            this.btnGiatribe4nho8.Location = new System.Drawing.Point(476, 165);
+            this.btnGiatribe4nho8.Name = "btnGiatribe4nho8";
+            this.btnGiatribe4nho8.Size = new System.Drawing.Size(275, 62);
+            this.btnGiatribe4nho8.TabIndex = 1;
+            this.btnGiatribe4nho8.Text = "Sắp xếp giá trị a > 4 & a < 8";
+            this.btnGiatribe4nho8.UseVisualStyleBackColor = true;
+            this.btnGiatribe4nho8.Click += new System.EventHandler(this.btnGiatribe4nho8_Click);
+            // 
+            // btnGiatritangdan
+            // 
+            this.btnGiatritangdan.Location = new System.Drawing.Point(253, 165);
+            this.btnGiatritangdan.Name = "btnGiatritangdan";
+            this.btnGiatritangdan.Size = new System.Drawing.Size(188, 62);
+            this.btnGiatritangdan.TabIndex = 1;
+            this.btnGiatritangdan.Text = "Sắp xếp giá trị a tăng đân";
+            this.btnGiatritangdan.UseVisualStyleBackColor = true;
+            this.btnGiatritangdan.Click += new System.EventHandler(this.btnGiatritangdan_Click);
+            // 
+            // btnGiatrilonhon4
+            // 
+            this.btnGiatrilonhon4.Location = new System.Drawing.Point(57, 165);
+            this.btnGiatrilonhon4.Name = "btnGiatrilonhon4";
+            this.btnGiatrilonhon4.Size = new System.Drawing.Size(142, 62);
+            this.btnGiatrilonhon4.TabIndex = 1;
+            this.btnGiatrilonhon4.Text = "Tìm giá trị a > 4";
+            this.btnGiatrilonhon4.UseVisualStyleBackColor = true;
+            this.btnGiatrilonhon4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtMangA, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtMangB, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtKetQua, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(921, 117);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(270, 39);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Mảng a";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(270, 39);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Mảng b";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Location = new System.Drawing.Point(3, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(270, 39);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Kết quả";
+            // 
+            // txtMangA
+            // 
+            this.txtMangA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMangA.Location = new System.Drawing.Point(279, 3);
+            this.txtMangA.Name = "txtMangA";
+            this.txtMangA.Size = new System.Drawing.Size(639, 22);
+            this.txtMangA.TabIndex = 3;
+            // 
+            // txtMangB
+            // 
+            this.txtMangB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMangB.Location = new System.Drawing.Point(279, 42);
+            this.txtMangB.Name = "txtMangB";
+            this.txtMangB.Size = new System.Drawing.Size(639, 22);
+            this.txtMangB.TabIndex = 4;
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtKetQua.Location = new System.Drawing.Point(279, 81);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(639, 22);
+            this.txtKetQua.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnManglonhon4);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.btnTimsvcomatrongdanhsach);
+            this.panel3.Controls.Add(this.btnChuoichuae);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 351);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1076, 248);
+            this.panel3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(473, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Linq với phương thức";
+            // 
+            // btnTimsvcomatrongdanhsach
+            // 
+            this.btnTimsvcomatrongdanhsach.Location = new System.Drawing.Point(403, 176);
+            this.btnTimsvcomatrongdanhsach.Name = "btnTimsvcomatrongdanhsach";
+            this.btnTimsvcomatrongdanhsach.Size = new System.Drawing.Size(252, 60);
+            this.btnTimsvcomatrongdanhsach.TabIndex = 2;
+            this.btnTimsvcomatrongdanhsach.Text = "Tìm sinh viên có mã trong danh sách";
+            this.btnTimsvcomatrongdanhsach.UseVisualStyleBackColor = true;
+            this.btnTimsvcomatrongdanhsach.Click += new System.EventHandler(this.btnTimsvcomatrongdanhsach_Click);
+            // 
+            // btnChuoichuae
+            // 
+            this.btnChuoichuae.Location = new System.Drawing.Point(417, 105);
+            this.btnChuoichuae.Name = "btnChuoichuae";
+            this.btnChuoichuae.Size = new System.Drawing.Size(227, 42);
+            this.btnChuoichuae.TabIndex = 1;
+            this.btnChuoichuae.Text = "Tìm chuỗi có ký tự e";
+            this.btnChuoichuae.UseVisualStyleBackColor = true;
+            this.btnChuoichuae.Click += new System.EventHandler(this.btnChuoichuae_Click);
+            // 
+            // btnManglonhon4
+            // 
+            this.btnManglonhon4.Location = new System.Drawing.Point(354, 35);
+            this.btnManglonhon4.Name = "btnManglonhon4";
+            this.btnManglonhon4.Size = new System.Drawing.Size(360, 49);
+            this.btnManglonhon4.TabIndex = 5;
+            this.btnManglonhon4.Text = "truy vấn dựa trên PT:  mảng lớn hơn 4";
+            this.btnManglonhon4.UseVisualStyleBackColor = true;
+            this.btnManglonhon4.Click += new System.EventHandler(this.btnManglonhon4_Click);
+            // 
+            // FromMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 599);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FromMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoadFrom);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnGiatricotrongA;
+        private System.Windows.Forms.Button btnGiatribe4nho8;
+        private System.Windows.Forms.Button btnGiatritangdan;
+        private System.Windows.Forms.Button btnGiatrilonhon4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMangA;
+        private System.Windows.Forms.TextBox txtMangB;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTimsvcomatrongdanhsach;
+        private System.Windows.Forms.Button btnChuoichuae;
+        private System.Windows.Forms.Button btnManglonhon4;
+    }
+}
+
---
+++ BaiTapLop/Tuan11/LinQtoObject/LinQtoObject/FromMain.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace LinQtoObject
+{
+    public partial class FromMain : Form
+    {
+        public FromMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void LoadFrom(object sender, EventArgs e)
+        {
+            txtMangA.Text = "4,6,-3,0,56,4,36,12,-98,34";
+            txtMangB.Text = "12,-4,67,23,99,66,36,18,0";
+        }
+
+        private int[] ChuyenDoi(TextBox txt)
+        {
+            try
+            {
+                return txt.Text.Split(',', (char)StringSplitOptions.RemoveEmptyEntries)
+                               .Select(s => int.Parse(s.Trim()))
+                               .ToArray();
+            }
+            catch
+            {
+                MessageBox.Show("Vui lòng nhập các số hợp lệ, cách nhau bằng dấu phẩy.");
+                return new int[0];
+            }
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] manga = ChuyenDoi(txtMangA);
+            var result = from i in manga
+                         where i > 4
+                         select i;
+            txtKetQua.Clear();
+            foreach (var i in result)
+            {
+                txtKetQua.Text = txtKetQua.Text + i.ToString() + ",";
+            }
+           
+        }
+
+        private void btnGiatritangdan_Click(object sender, EventArgs e)
+        {
+            int[] manga = ChuyenDoi(txtMangA);
+            var result = from i in manga
+                         orderby i
+                         select i;
+            txtKetQua.Clear();
+            foreach (var i in result)
+            {
+                txtKetQua.Text = txtKetQua.Text + i.ToString() + ",";
+            }
+        }
+
+        private void btnGiatribe4nho8_Click(object sender, EventArgs e)
+        {
+            int[] manga = ChuyenDoi(txtMangA);
+          
+            var result = from i in manga
+                         where i > 4 && i < 8
+                         select i;
+            txtKetQua.Clear();
+            txtKetQua.Clear();
+            foreach (var i in result)
+            {
+                txtKetQua.Text = txtKetQua.Text + i.ToString() + ",";
+            }
+        }
+
+        private void btnGiatricotrongA_Click(object sender, EventArgs e)
+        {
+            int[] a = ChuyenDoi(txtMangA);
+            int[] b = ChuyenDoi(txtMangB);
+           
+            var result = from i in a
+                         from j in b
+                         where i == j
+                         select i;
+            txtKetQua.Clear();
+            foreach (var i in result)
+            {
+                txtKetQua.Text = txtKetQua.Text + i.ToString() + ",";
+            }
+        }
+
+        private void btnManglonhon4_Click(object sender, EventArgs e)
+        {
+            int[] a = ChuyenDoi(txtMangA);
+            var result = a.Where(x => x > 4);
+            txtKetQua.Clear();
+            foreach (var i in result)
+            {
+                txtKetQua.Text = txtKetQua.Text + i.ToString() + ",";
+            }
+        }
+
+        private void btnChuoichuae_Click(object sender, EventArgs e)
+        {
+           
+            ArrayList chuoi = new ArrayList()
+    {
+        "zero", "one", "two", "four", "five",
+        "six", "seven", "eight", "nine", "ten"
+    };
+
+            
+            txtMangB.Text = string.Join(", ", chuoi.Cast<string>());
+
+            
+            var result = chuoi.Cast<string>()
+                              .Where(s => s.Contains("e"));
+
+            
+            txtKetQua.Text = string.Join(", ", result);
+        }
+
+
+        private void btnTimsvcomatrongdanhsach_Click(object sender, EventArgs e)
+        {
+            ArrayList listSv = new ArrayList();
+
+            listSv.Add(new SinhVienDTO("sv01", "nguyen minh thuan", "099999555", "thuan@gmail.com", "binh chanh"));
+            listSv.Add(new SinhVienDTO("sv02", "le van hoang", "091234567", "hoang@gmail.com", "quan 1"));
+            listSv.Add(new SinhVienDTO("sv03", "tran thi my linh", "098765432", "mylinh@gmail.com", "tan binh"));
+            listSv.Add(new SinhVienDTO("sv04", "pham van loc", "093333444", "locpham@gmail.com", "go vap"));
+            listSv.Add(new SinhVienDTO("sv05", "nguyen hoai nam", "097777888", "hoainam@gmail.com", "thu duc"));
+            listSv.Add(new SinhVienDTO("sv06", "doan quoc bao", "095555666", "quocbao@gmail.com", "binh thanh"));
+
+            string maCantim = txtMangB.Text.Trim();
+
+            
+            var result = listSv.Cast<SinhVienDTO>()
+                               .Where(sv => sv.Masv == maCantim)
+                               .Select(sv => $"{sv.Masv} - {sv.TenSv} - {sv.Sdt} - {sv.Email} - {sv.DiaChi}");
+
+            
+            if (!result.Any())
+            {
+                txtKetQua.Text = "Không tìm thấy sinh viên!";
+                return;
+            }
+
+            txtKetQua.Text = string.Join("\r\n", result);
+        }
+
+    }
+}
